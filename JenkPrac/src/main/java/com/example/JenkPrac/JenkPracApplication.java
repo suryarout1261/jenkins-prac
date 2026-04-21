96ceeeefd8d3423eea3cbc740092fa3a761b8165
Surya Prasad Rout <surya_prasadrout@epam.com>
2026-04-21 17:13:01 +0530

    updated files

---
+++ JenkPrac/src/main/java/com/example/JenkPrac/JenkPracApplication.java
@@ -0,0 +1,17 @@
+package com.example.JenkPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkPracApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JenkPracApplication.class, args);
+		System.out.println("Hello surya!!!");
+
+
+	}
+
+}
---
+++ JenkPrac/src/test/java/com/example/JenkPrac/JenkPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkPrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
